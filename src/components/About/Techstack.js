c8376b2ccc2af06fafccb54c8b8d5c0ef08256d5
S Adithya <157592574+AnbudanAdithya@users.noreply.github.com>
2025-07-14 16:05:10 +0000

    ABOUT PAGE SKILL UPDATE

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { DiPython } from "react-icons/di";
 import { SiMicrosoftsqlserver } from "react-icons/si";
+import { SiTableau } from "react-icons/si";
 import { FaFileExcel, FaFilePowerpoint, FaFileWord } from "react-icons/fa";
 import { SiCanva } from "react-icons/si";
 
@@ -26,6 +27,9 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCanva />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
       </Col>
     </Row>
   );
