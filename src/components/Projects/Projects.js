99dcbdaf322695d96df1a703782727afbc6b610b
S Adithya <157592574+AnbudanAdithya@users.noreply.github.com>
2025-08-04 16:07:26 +0000

    New Project & Google Analytics

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Report from "../../Assets/Projects/PDF_Files/FinTech_Report.jpg";
 import BR from "../../Assets/Projects/PDF_Files/Book_Review.png";
 import EMI from "../../Assets/Projects/Excel_Files/EMI_Model.jpg";
 import Tax from "../../Assets/Projects/Excel_Files/Tax_Calculator.jpg";
+import BA from "../../Assets/Projects/Drive/BA.png";
 
 function Projects() {
   return (
@@ -24,7 +25,6 @@ function Projects() {
                 isBlog={false}
                 title="EMI Calulator"
                 description="A simple yet powerful, user friendly excel model that lets user calculate the loan EMI to be paid."
-                //ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
                 demoLink="/Assets/Project/EMI_Calculator.xlsm"
               />
             </Col>
@@ -37,7 +37,6 @@ function Projects() {
                 isBlog={false}
                 title="Tax Calcutor"
                 description="Based on new budget as announced by Hon'ble Finance Minister, I tried to create a model to identify the tax payable."
-                //ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
                 demoLink="/Assets/Project/Tax_Calculator.xlsm"
               />
             </Col>
@@ -50,7 +49,6 @@ function Projects() {
                 isBlog={false}
                 title="Book Review - Psychology of Money"
                 description="Take a look at my book review I have shared my thoughts on the book 'Psychology of Money' by Morgan Housel."
-                //ghLink="https://github.com/soumyajit4419/Editor.io"
                 demoLink="/Assets/Project/Book_Review.pdf"
               />
             </Col>
@@ -63,8 +61,19 @@ function Projects() {
                 isBlog={false}
                 title="FinTech Banks - Brief Study Report"
                 description="A brief study report on the FinTech Banks in India, covering their impact, growth, and future prospects."
-                //ghLink="https://github.com/soumyajit4419/Plant_AI"
                 demoLink="/Assets/Project/FinTech_Banks.pdf"
+              />
+            </Col>
+          )}
+
+          {true && (
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={BA}
+                isBlog={false}
+                title="Bike Sales - Predictive Analytics using R Studio and Power BI"
+                description="A model and a dashboard predicting sales of bike in India based on different criteria with a comprehensive report; Dataset adopted from Kaggle."
+                demoLink="https://drive.google.com/drive/folders/1TrKx-nA0NzJck2LAvIH-b78XGXDh8dyt?usp=drive_link"
               />
             </Col>
           )}
